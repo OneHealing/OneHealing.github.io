752799d9b5f662d4f764384f1f0d8e0c1277b60f
OneHealing <102yusron@gmail.com>
2023-05-08 10:51:45 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function satu() {
+    setTimeout(() => {
+        console.log('1')
+    }, 1000)
+}
+function dua() {
+    setTimeout(() => {
+        console.log('2')
+    }, 2000)
+}
+function tiga() {
+    setTimeout(() => {
+        console.log('3')
+    }, 3000)
+}
+
+satu()
+dua()
+tiga()
